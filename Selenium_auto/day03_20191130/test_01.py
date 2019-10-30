47cee364f65bc71704befdce88b376b9dcefa367
azhenglianxi <azhenglianxi@163.com>
2019-10-30 22:25:08 +0800

    1

--- Selenium_auto/day03_20191130/test_01.py
+++ Selenium_auto/day03_20191130/test_01.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 #@Time     :2019-10-30 17:22:08   #@Author   :azhenglianxi   
 #@Software :PyCharm
+from Selenium_auto.funtion_Interface import *
+#Selenium 等待元素出现  + frame 界面切换 +css
 
-#Selenium 等待元素出现  + frame 界面切换 +css 选择器
 
+
+
+
+
+#css 定位选择器
+driver =ChromeDrverNObroews()
+driver.get()
+driver.switch_to.frame()
